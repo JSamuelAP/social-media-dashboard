dff48c0befc72396f41a80ec42aaf02e40258f74
JSamuel <sp4619168@gmail.com>
2023-01-12 11:38:19 -0600

    feat: Grid de 1 columna para pantallas mobile

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -2,11 +2,27 @@ const MainContent = () => {
 	return (
 		<main class="main">
 			<div className="container">
-				@nathanf 1987 Followers 12 Today @nathanf 1044 Followers 99 Today
-				@realnathanf 11k Followers 1099 Today Nathan F. 8239 Subscribers 144
-				Today Overview - Today Page Views 87 3% Likes 52 2% Likes 5462 2257%
-				Profile Views 52k 1375% Retweets 117 303% Likes 507 553% Likes 107 19%
-				Total Views 1407 12%
+				<section className="social-media">
+					<div className="grid social__media-grid">
+						<article>@nathanf 1987 Followers 12 Today </article>
+						<article>@nathanf 1044 Followers 99 Today</article>
+						<article>@realnathanf 11k Followers 1099 Today </article>
+						<article>Nathan F. 8239 Subscribers 144 Today</article>
+					</div>
+				</section>
+				<section className="overview">
+					<h2>Overview - Today</h2>
+					<div className="grid overview__grid">
+						<article>Page Views 87 3%</article>
+						<article>Likes 52 2%</article>
+						<article>Likes 5462 2257%</article>
+						<article>Profile Views 52k 1375%</article>
+						<article>Retweets 117 303%</article>
+						<article>Likes 507 553%</article>
+						<article>Likes 107 19%</article>
+						<article>Total Views 1407 12%</article>
+					</div>
+				</section>
 			</div>
 		</main>
 	);
