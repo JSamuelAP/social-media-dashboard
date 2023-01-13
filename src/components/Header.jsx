84635ecbbd6308d9e36231703aef1845b1d22f94
JSamuel <sp4619168@gmail.com>
2023-01-13 12:03:11 -0600

    feat: Ajustar el layout a pantallas mas largas
    Se ajusto el header y se subio el contenido principal y el footer.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Switch from "./Switch";
 const Header = () => {
 	return (
 		<header className="header">
-			<div className="container">
+			<div className="container header__container">
 				<div>
 					<h1 className="header__heading">Social Media Dashboard</h1>
 					<span className="header__followers">Total Followers: 23,004</span>
