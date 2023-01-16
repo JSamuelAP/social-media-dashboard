71b3623ca1ce83f669cd6899b728fe9729c6e535
JSamuel <sp4619168@gmail.com>
2023-01-16 15:42:42 -0600

    feat: Objeto con todas las estadisticas creado
    Todas las estadisticas de las redes sociales las provee el contexto.
    Se renderizan dinamicamente las cards de las estadisticas.

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,117 +1,57 @@
+import useSocialMedia from "../hooks/useSocialMedia";
 import Card from "./Card";
 import CardToday from "./CardToday";
 
 const MainContent = () => {
+	const { socialMedia, overviewToday } = useSocialMedia();
+
 	return (
 		<main class="main">
 			<div className="container">
 				<section className="social-media">
 					<div className="grid social-media__grid">
-						<Card
-							data={{
-								socialMedia: "facebook",
-								username: "@nathanf",
-								numFollowersTotal: "1987",
-								followersText: "Followers",
-								numFollowersToday: 12,
-							}}
-						/>
-						<Card
-							data={{
-								socialMedia: "twitter",
-								username: "@nathanf",
-								numFollowersTotal: "1044",
-								followersText: "Followers",
-								numFollowersToday: 99,
-							}}
-						/>
-						<Card
-							data={{
-								socialMedia: "instagram",
-								username: "@realnathanf",
-								numFollowersTotal: "11k",
-								followersText: "Followers",
-								numFollowersToday: 1099,
-							}}
-						/>
-						<Card
-							data={{
-								socialMedia: "youtube",
-								username: "Nathan F.",
-								numFollowersTotal: "8239",
-								followersText: "Subscribers",
-								numFollowersToday: -144,
-							}}
-						/>
+						{socialMedia.map((item) => {
+							const {
+								name,
+								username,
+								totalFollowers,
+								typeFollowers,
+								followersToday,
+							} = item;
+
+							return (
+								<Card
+									key={name}
+									data={{
+										socialMedia: name,
+										username,
+										numFollowersTotal: totalFollowers,
+										followersText: typeFollowers,
+										numFollowersToday: followersToday,
+									}}
+								/>
+							);
+						})}
 					</div>
 				</section>
 				<section className="overview">
 					<h2 className="overview__heading">Overview - Today</h2>
 					<div className="grid overview__grid">
-						<CardToday
-							data={{
-								title: "Page Views",
-								icon: "facebook",
-								number: "87",
-								percentage: 3,
-							}}
-						/>
-						<CardToday
-							data={{
-								title: "Likes",
-								icon: "facebook",
-								number: "52",
-								percentage: 2,
-							}}
-						/>
-						<CardToday
-							data={{
-								title: "Likes",
-								icon: "instagram",
-								number: "5462",
-								percentage: 2257,
-							}}
-						/>
-						<CardToday
-							data={{
-								title: "Profile Views",
-								icon: "instagram",
-								number: "52k",
-								percentage: 1375,
-							}}
-						/>
-						<CardToday
-							data={{
-								title: "Retweets",
-								icon: "twitter",
-								number: "117",
-								percentage: 303,
-							}}
-						/>
-						<CardToday
-							data={{
-								title: "Likes",
-								icon: "twitter",
-								number: "507",
-								percentage: 553,
-							}}
-						/>
-						<CardToday
-							data={{
-								title: "Likes",
-								icon: "youtube",
-								number: "107",
-								percentage: -19,
-							}}
-						/>
-						<CardToday
-							data={{
-								title: "Total Views",
-								icon: "youtube",
-								number: "1407",
-								percentage: -12,
-							}}
-						/>
+						{overviewToday.map((item, index) => {
+							const { title, name, number, percentage } = item;
+
+							return (
+								<CardToday
+									key={index}
+									data={{
+										title,
+										icon: name,
+										number,
+										percentage,
+									}}
+								/>
+							);
+						})}
 					</div>
 				</section>
 			</div>
--- src/context/SocialMediaContext.jsx
+++ src/context/SocialMediaContext.jsx
@@ -4,6 +4,86 @@ const SocialMediaContext = createContext();
 
 const initialSocialMediaStats = {
 	totalFollowers: "23,004",
+	socialMedia: [
+		{
+			name: "facebook",
+			username: "@nathanf",
+			totalFollowers: "1987",
+			typeFollowers: "followers",
+			followersToday: 12,
+		},
+		{
+			name: "twitter",
+			username: "@nathanf",
+			totalFollowers: "1044",
+			typeFollowers: "followers",
+			followersToday: 99,
+		},
+		{
+			name: "instagram",
+			username: "@realnathanf",
+			totalFollowers: "11k",
+			typeFollowers: "followers",
+			followersToday: 1099,
+		},
+		{
+			name: "youtube",
+			username: "Nathan F.",
+			totalFollowers: "8239",
+			typeFollowers: "subscribers",
+			followersToday: -144,
+		},
+	],
+	overviewToday: [
+		{
+			name: "facebook",
+			title: "Page Views",
+			number: "87",
+			percentage: 12,
+		},
+		{
+			name: "facebook",
+			title: "Likes",
+			number: "52",
+			percentage: -2,
+		},
+		{
+			name: "instagram",
+			title: "Likes",
+			number: "5462",
+			percentage: 2257,
+		},
+		{
+			name: "instagram",
+			title: "Profile Views",
+			number: "52k",
+			percentage: 1375,
+		},
+		{
+			name: "twitter",
+			title: "Retweets",
+			number: "117",
+			percentage: 303,
+		},
+		{
+			name: "twitter",
+			title: "Likes",
+			number: "507",
+			percentage: 553,
+		},
+		{
+			name: "youtube",
+			title: "likes",
+			number: "107",
+			percentage: -19,
+		},
+		{
+			name: "youtube",
+			title: "Total Views",
+			number: "1407",
+			percentage: -12,
+		},
+	],
 };
 
 const SocialMediaProvider = ({ children }) => {
