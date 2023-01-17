93bea9ad3f9c589f49d12446c87ba0bfb87bc54d
JSamuel <sp4619168@gmail.com>
2023-01-17 16:40:54 -0600

    Updates

--- src/hooks/useSocialMedia.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import SocialDataContext from "../context/SocialDataContext";
-
-const useSocialData = () => {
-	return useContext(SocialDataContext);
-};
-
-export default useSocialData;
